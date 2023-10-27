69d2c8c903bd9f4cc8ca705b37aa2f9cdbae2395
Ali Hamza <officer3765@gmail.com>
2023-10-27 15:42:01 +0500

    Initial

---
+++ app/src/main/java/com/aitechnologies/roomdatabase/ActivityDetail.kt
@@ -0,0 +1,11 @@
+package com.aitechnologies.roomdatabase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
